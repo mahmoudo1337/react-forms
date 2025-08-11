cd766e9a00061592f9b13e104d94805a37642155
mahmoudo1337 <rickgrimestwd1337@gmail.com>
2025-08-11 20:23:24 +0300

    adding submission based validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,26 +4,35 @@ export default function Login() {
   const email = useRef();
   const password = useRef();
 
-  const [enteredValue, setEnteredValue] = useState({
-    email: "",
-    password: "",
-  });
+  const [emailIsInvalid, setEmailIsInvalid] = useState();
 
   function handleSubmit(event) {
     event.preventDefault();
+
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
+
     console.log(enteredEmail, enteredPassword);
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+    setEmailIsInvalid(false);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} noValidate>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
